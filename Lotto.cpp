702f168ebe1499d418b71f811b1aaf6f9439bd78
batidom <152321640+batidom@users.noreply.github.com>
2024-01-26 02:17:33 +0100

    Add files via upload

---
+++ Lotto.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <windows.h>
+#include <ctime>
+
+using namespace std;
+
+
+int main() {
+	string starter;
+	cout << "Program, ktory symuluje losowanie LOTTO. " << endl;
+	cout << "Aby rozpoczac losowanie wprowadz dowolny znak i zatwierdz ENTER... " << endl << endl;
+	cin >> starter;
+	system("cls");
+	cout << "Oto wyniki dzisiejszego losowania: " << endl << endl;
+	int liczba[6], t;
+	string input;
+start:
+	for (int i = 1; i < 7; i++){
+		srand(t = time(0));
+losowanie: 
+		liczba[i] = rand() % 49;
+		if(liczba[i] == 0 || liczba[i] == liczba[i-1] || liczba[i] == liczba[i-2] || liczba[i] == liczba[i-3] || liczba[i] == liczba[i-4] || liczba[i] == liczba[i-5]){
+		i--;
+		continue;}
+		cout << "\tLiczba nr: " << i << "\tWylosowana liczba to: " << liczba[i] << endl;
+		Sleep(3000);
+}
+
+cout << "\nDziekujemy za udzial w losowaniu :)" << endl;
+cout << "\nPonowic losowanie? Wprowadz dowolny znak, zatwierdz ENTER'em. " << endl;
+cout << "Lub jezeli chcesz wyjsc napisz EXIT. " << endl;
+
+cin >> input;
+if (input == "EXIT"){
+	return 0;
+}
+system("cls");
+goto start;
+
+	return 0;
+}
